c10822a0bc776289bfa17da71333651a1de59af9
Natalia Braz <naabraz@gmail.com>
2020-11-06 23:26:05 -0300

    chore(mail service): add unit test

--- src/service/mail/index.test.ts
+++ src/service/mail/index.test.ts
@@ -1,13 +1,16 @@
 import { createTransport } from 'nodemailer';
 import { mocked } from 'ts-jest/utils';
 
+import logger from 'helpers/logger';
 import { sendEmail } from './index';
 
 jest.mock('nodemailer');
+
 jest.mock('config', () => ({
   EMAIL_SERVICE: 'EMAIL_SERVICE',
   OAUTH_USER: 'OAUTH_USER'
 }))
+
 jest.mock('helpers/logger', () => ({
   error: jest.fn(),
   info: jest.fn(),
@@ -18,7 +21,7 @@ describe('Given mail service', () => {
 
   mockedCreateTransport.mockReturnValue(({
     ...require('nodemailer'),
-    sendMail: jest.fn((_, callback) => callback()),
+    sendMail: jest.fn((_, callback) => callback(null, 'response')),
     close: jest.fn(),
   }))
 
@@ -39,19 +42,33 @@ describe('Given mail service', () => {
   }
 
   it('Should call createTransport nodemailer method', () => {
-    const mockedSmtpTransport = {
-      "auth": {
-          clientId: "clientId",
-          clientSecret: "clientSecret",
-          refreshToken: "refreshToken",
-          type: "OAuth2",
-          user: "OAUTH_USER"
-        },
-        service: "EMAIL_SERVICE"
-      }
+  const mockedSmtpTransport = {
+    auth: {
+        clientId: "clientId",
+        clientSecret: "clientSecret",
+        refreshToken: "refreshToken",
+        type: "OAuth2",
+        user: "OAUTH_USER"
+      },
+      service: "EMAIL_SERVICE"
+    }
 
     sendEmail(oAuth, mail)
 
     expect(mockedCreateTransport).toHaveBeenCalledWith(mockedSmtpTransport)
+    expect(mockedCreateTransport().close).toHaveBeenCalled()
+  })
+
+  it('Should call logger method when createTransport returns error method', () => {
+    mockedCreateTransport.mockReturnValue(({
+      ...require('nodemailer'),
+      sendMail: jest.fn((_, callback) => callback('error', null)),
+      close: jest.fn(),
+    }))
+
+    sendEmail(oAuth, mail)
+
+    expect(logger.error).toHaveBeenCalledWith('error')
+    expect(mockedCreateTransport().close).toHaveBeenCalled()
   })
 })
