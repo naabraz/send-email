0fe9cbae993b1be5b8c94a15d59f86c701ae05b8
Natalia Braz <naabraz@gmail.com>
2020-11-07 01:06:52 -0300

    fix(tsconfig): add types

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
 import 'dotenv/config';
 import 'module-alias/register';
 
-import express, { Application } from 'express';
+import express from 'express';
 
 import { mail } from 'controller/mail';
 import logger from 'helpers/logger';
 
-const app: Application = express();
+const app: express.Application = express();
 const port = 3000;
 
 app.use(express.json());
