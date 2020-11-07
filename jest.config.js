759540bc14d73e892ff2ca74e6d4b3ca0a6e8ed0
Natalia Braz <naabraz@gmail.com>
2020-11-06 23:12:02 -0300

    chore(unit test): add unit test

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,11 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   moduleNameMapper: {
-    '^src/(.*)$': '<rootDir>src/$1',
+    '^config(.*)$': '<rootDir>/src/config$1',
+    '^controller(.*)$': '<rootDir>/src/controller$1',
+    '^helpers(.*)$': '<rootDir>/src/helpers$1',
+    '^service(.*)$': '<rootDir>/src/service$1',
+    '^types(.*)$': '<rootDir>/src/types$1',
   },
   collectCoverageFrom: [
     'src/**/*.ts',
--- src/service/mail/index.test.ts
+++ src/service/mail/index.test.ts
@@ -1,5 +1,57 @@
+import { createTransport } from 'nodemailer';
+import { mocked } from 'ts-jest/utils';
+
+import { sendEmail } from './index';
+
+jest.mock('nodemailer');
+jest.mock('config', () => ({
+  EMAIL_SERVICE: 'EMAIL_SERVICE',
+  OAUTH_USER: 'OAUTH_USER'
+}))
+jest.mock('helpers/logger', () => ({
+  error: jest.fn(),
+  info: jest.fn(),
+}))
+
 describe('Given mail service', () => {
+  const mockedCreateTransport = mocked(createTransport, true)
+
+  mockedCreateTransport.mockReturnValue(({
+    ...require('nodemailer'),
+    sendMail: jest.fn((_, callback) => callback()),
+    close: jest.fn(),
+  }))
+
+  const oAuth = {
+    accessToken: new Promise((resolve, _) => resolve(true)),
+    clientSecret: 'clientSecret',
+    clientId: 'clientId',
+    refreshToken: 'refreshToken',
+  }
+
+  const mail = {
+    from: 'from',
+    to: 'string',
+    replyTo: 'string',
+    subject: 'string',
+    generateTextFromHTML: true,
+    html: 'string',
+  }
+
   it('Should call createTransport nodemailer method', () => {
-    expect(1+1).toEqual(2)
+    const mockedSmtpTransport = {
+      "auth": {
+          clientId: "clientId",
+          clientSecret: "clientSecret",
+          refreshToken: "refreshToken",
+          type: "OAuth2",
+          user: "OAUTH_USER"
+        },
+        service: "EMAIL_SERVICE"
+      }
+
+    sendEmail(oAuth, mail)
+
+    expect(mockedCreateTransport).toHaveBeenCalledWith(mockedSmtpTransport)
   })
 })
